3236706c0851e49f9ced710b1d93951b19981159
stanleysun233 <stanleysun233@163.com>
2021-10-26 23:09:38 +0800

    Upload Code

---
+++ LessonTwo/proj/cmake-build-debug/CMakeFiles/proj.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proj.dir/ADT.cpp.obj"
+  "CMakeFiles/proj.dir/main.cpp.obj"
+  "proj.exe"
+  "proj.exe.manifest"
+  "proj.lib"
+  "proj.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
