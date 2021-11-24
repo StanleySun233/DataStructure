ee64fafb00883e1aad812099e9020632906a1448
Stanley Sun <75790014+StanleySun233@users.noreply.github.com>
2021-11-24 16:46:17 +0800

    Update main.cpp

--- LessonSix/proj1/main.cpp
+++ LessonSix/proj1/main.cpp
@@ -15,29 +15,3 @@ int main()
     MGraphBFS(G);
     return 0;
 }
-
-/* case 0
-
-5 7 0
-ABCDE
-0 1
-0 4
-1 2
-2 3
-3 0
-3 1
-4 2
-*/
-
-/* case 1
-
-6 7 2
-ABCDEF
-0 1
-0 4
-1 4
-1 5
-2 3
-2 5
-3 5
-*/
